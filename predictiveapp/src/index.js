5c1af464e2b7539238fb75b4dde3cb5861efbbba
Pierre <pierre.vauterin@ensiie.fr>
2025-02-25 19:49:47 +0100

    Erased Hello world component

--- predictiveapp/src/index.js
+++ predictiveapp/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-function HelloWorld()
-{
-  return <h1 className="greeting">Hello world!</h1>;
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
